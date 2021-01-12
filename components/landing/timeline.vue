1411f16e275cae53f125efe585ba7e350c6b8436
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2021-01-12 13:29:36 +0700

    Add about us

--- components/landing/timeline.vue
+++ components/landing/timeline.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="pa-2">
+    <v-row dense>
+      <v-col lg="6" md="6" sm="12" cols="12">
+        <v-card class="content-card" flat tile :outlined="false">
+          <v-card-title class="justify-center text-center"
+            ><h3 style="font-size: 1rem">Hasna Karimah, S.Kom</h3></v-card-title
+          >
+          <v-card-text
+            class="justify-center text-center"
+            style="font-size: 0.8rem"
+          >
+            <div>
+              Putri Bungsu dari Bapak Dr. Ir. H, Yusuf Afandi, M.T dan Ibu Dra.
+              Hj. Nurhasanah Dewi. Lahir di Tangerang, 15 September 1998.
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col lg="6" md="6" sm="12" cols="12"
+        ><v-card class="content-card" flat tile :outlined="false">
+          <v-card-title class="justify-center text-center"
+            ><h3 style="font-size: 1rem">
+              Rahmatullah Arrizal Pranatadesta, S.Kom, M.T
+            </h3></v-card-title
+          >
+          <v-card-text
+            class="justify-center text-center"
+            style="font-size: 0.8rem"
+          >
+            <div>
+              Putra Sulung dari Bapak Prof. Dr. H. Engkus Kuswarno, M.S dan Hj.
+              Mia Rachmiati, A.Md. Lahir di Bandung, 4 April 1996.
+            </div>
+          </v-card-text>
+        </v-card></v-col
+      >
+    </v-row>
     <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
       <v-timeline-item
         v-for="(item, i) in items"
